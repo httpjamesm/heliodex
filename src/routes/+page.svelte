ae4d978979bc1cfc3c7bc8cfb2a07412ea86db1f
httpjamesm <github@httpjames.space>
2024-12-04 06:52:02 -0500

    feat: route on app entry

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import { Pages } from "$lib/pages";
+
+  onMount(() => {
+    if (window.localStorage.getItem("onboarding")) {
+      goto(Pages.Home);
+    } else {
+      goto(Pages.Landing);
+    }
+  });
+</script>
